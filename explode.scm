8a91b4664b7d01d184d467b3bdb9b66cbf519112
Todor Slavov <todor.slavov2818@gmail.com>
2024-07-30 16:13:54 +0200

    Fill let bindings HT. Explode/flatten definitions on each acyclic. Generate combinations (DFS).

--- explode.scm
+++ explode.scm
@@ -3,7 +3,11 @@
 (import (scheme small)
         (srfi 193) ; command-args
         (srfi 166)
+	(srfi 125) ; hash-table
+	(srfi 128) ; symbol-hash
+	(srfi 1)
         (rebottled packrat)
+	(chibi match)
         (cat parser))
 
 (define (pretty-print x)
@@ -37,11 +41,58 @@
                       (iter rest (append istmts nstmts)))
                     (begin
                       (print "WARNING: cannot include '" (cadr stmt) "'")
-                      (iter rest nstmts)))
-                (iter rest (cons stmt nstmts))))))
+                      (iter rest nstmts))
+		)
+                (iter rest (cons stmt nstmts))
+	    )
+          )
+    )
+  )
     (list 'model (cadr model) (reverse (iter stmts '())))))
+        
+(define (get-hash-table model)
+  (let ((ht (make-hash-table equal? symbol-hash 1024))
+        (stmts (caddr model)))
+        (for-each (lambda (stmt)
+          (when (eq? 'let (car stmt)) 
 
-(define (main args)
+      (let ((label (cadr stmt))
+            (expr (caddr stmt)))
+	   (hash-table-set! ht label expr)))) 
+        stmts)
+      ht))
+
+(define (explode-expr expr ht)
+  (define (explode expr)
+    (match expr
+           (('union . exprs) (apply append (map explode exprs)))
+            (('rel . label) (if (hash-table-exists? ht label)
+	                       (explode (hash-table-ref ht label))
+			       (list expr)))
+            (else `(,expr))
+))
+  (explode expr))
+
+(define (explode-accs model ht)                                  
+  (let* ((stmts (caddr model))
+         (accs (filter (lambda (x) (eq? (car x) 'acyclic)) stmts)))
+    (apply append (map (lambda (acc) (explode-expr (cadr acc) ht)) accs))))
+
+(define (dfs edges path d)
+  (if (eq? d 0) 
+    (list path)
+    (apply append (map (lambda (edge) 
+	(dfs edges (append path (list (cdr edge))) (- d 1))
+    ) edges))))
+
+(define (cycles-print cycles)
+  (define (print-plus cycle)
+    (display cycle)
+    (display " ")
+  )
+ (for-each (lambda (cycle) (for-each (lambda (el) (print-plus el)) cycle) (newline)) cycles))
+
+(define (main args)  
   (die-unless (= (length args) 2) "wrong arguments")
 
   ; car , cdr
@@ -54,18 +105,21 @@
          (len (car (cdr args)))
          (len (string->number len))
          (port (open-input-file fn)))
-    (print "model file: " fn)
-    (print "cycle len: " len)
+    (print "# model file: " fn)
+    (print "# cycle len: " len)
 
     (let ((tokens (tokenize-cat fn)))
-      (pretty-print tokens)
-      (newline)
-      (display "-------------------------------------------------")
-      (newline)
+      ;(pretty-print tokens)
+      ;(newline)
+      ;(display "-------------------------------------------------")
+      ;(newline)
       (let* ((model (parse-cat tokens))
-             (model (include-files model)))
-
-        (pretty-print model)
+             (model (include-files model))
+	     (ht (get-hash-table model)))	
+        (let* ((edges (explode-accs model ht))
+	      (cycles (dfs edges '() len)))
+	    (cycles-print cycles) 
+	)
 
         )))
   0)
