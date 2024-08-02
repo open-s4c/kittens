c75b1d5f8143c5dc1e342556c889bb6f76430064
Todor Slavov <todor.slavov2818@gmail.com>
2024-08-02 09:41:55 +0200

    Now really all but the "exists(...)".

--- roast.scm
+++ roast.scm
@@ -19,7 +19,7 @@
 
 (define-record-type 
   event-record 
-  (event eid tid addr op) 
+  (event eid tid addr val op) 
   event? 
   (eid event-eid) (tid event-tid) (addr event-addr) (val event-val) (op event-op))
 
@@ -85,17 +85,21 @@
 		        (else
 			       (loop (cdr coll) cnt)))))
 
+(define (get-read-t-number event event-records) 
+  (apply string-append `(
+	       ,(number->string (
+                  count (lambda (ev) (and (eq? (event-op ev) 'read) (< (event-eid ev) (event-eid event)))) event-records
+ )))))
+
 (define (print-event event event-records)
   (apply string-append `(
     "    "
     ,(if (eq? (event-op event) 'read) 
        (apply string-append `(
          ,(string-append "int r" 
-	     (apply string-append `(
-	       ,(number->string (
-                  count (lambda (ev) (and (eq? (event-op ev) 'read) (< (event-eid ev) (event-eid event)))) event-records
-	       ))
-	     ))
+	     (
+                get-read-t-number event event-records
+	     )
 	   " = *");
          ,(number-to-alphabet-string (event-addr event))
 	 ";"
@@ -105,7 +109,7 @@
          "*"
 	 ,(number-to-alphabet-string (event-addr event))
 	 " = "
-	 
+	 ,(number->string (event-val event))
 	 ";"
          )
        )
@@ -128,43 +132,53 @@
   )
 )
 
+(define (generate-header)
+  (apply string-append '(
+    "Test Name \n"
+    "Some Very Useful Information\n"
+    "{}\n\n"
+  ))
+)
+
+(define (generate-assert event-records)
+  (apply string-append `(
+    "exists("
+    "..."
+    ")\n"
+  ))
+)
+
 (define (generate-litmus-PC event-records)
   (apply string-append `(
-      "{}n"
-      ,@(apply append ( map (lambda (event-records-per-tid) (list (generate-thread-code event-records-per-tid) "\n")) event-records))
-      "exists()"
+      ,(generate-header)
+      ,@(apply append (map (lambda (event-records-per-tid) (list (generate-thread-code event-records-per-tid) "\n")) event-records))
+      ,(generate-assert event-records)
     ))
 )
 
- (main args)
-  (let ((my-model 
-	          '(model 
-                     (define-fun ev1 () Event
-		       (mk-event 1 3 101 write))
-		     (define-fun ev2 () Event
-		       (mk-event 2 2 102 read))))
-        (my0model
+(define (main args)
+  (let ((my-model
 		  '(model
 		     (define-fun ev1 () Event
 		       (mk-event 1 0 102 12 read))
 		     (define-fun ev3 () Event
-		       (mk-event 3 2 103 16 read))
+		       (mk-event 3 0 104 16 read))
 		     (define-fun ev2 () Event
-		       (mk-event 2 2 102 13 write))
+		       (mk-event 2 0 102 13 write))
 		     (define-fun ev0 () Event
-		       (mk-event 0 0 103 15 write)))))
+		       (mk-event 0 2 103 13 write))
+		     (define-fun ev2 () Event
+		       (mk-event 5 2 104 13 read))
+		     (define-fun ev0 () Event
+		       (mk-event 4 2 103 15 write)))))
 
     (let* ((event-records (extract-event-records my-model))
            (tids (get-tids event-records))
 	   (events-per-tid (records-per-tid event-records tids))
 	   (records-sorted (sort-records events-per-tid)))
-      (newline)
-      (display records-sorted)
-      (newline)
                
       (display (generate-litmus-PC records-sorted))         
 
-      (newline)
 )))
 
 (main (command-args))
