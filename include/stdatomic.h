fcc95bdf83ba30a3daa184b7d26e942dc98bcbf6
Diogo Behrens <diogo.behrens@huawei.com>
2024-08-26 14:00:17 +0200

    Add stdatomic mapping to vatomic
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ include/stdatomic.h
@@ -0,0 +1,15 @@
+#ifndef _STDATOMIC_H_
+#define _STDATOMIC_H_
+
+#include <vsync/atomic.h>
+#include <vsync/atomic/dispatch.h>
+
+/* vatomic32_t and vatomic64_t types are unsigned integers. Nevertheless,
+ * for the kind of test cases generated here, this should be fine. */
+#define atomic_int vatomic32_t
+#define atomic_long vatomic64_t
+
+#define atomic_exchange_explicit(a, v, mo) vatomic_xchg(a, v)
+#define atomic_store_explicit(a, v, mo) vatomic_write(a, v)
+
+#endif
